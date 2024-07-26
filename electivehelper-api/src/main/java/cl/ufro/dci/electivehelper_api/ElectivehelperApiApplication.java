b846daf3d596ffce7c5b331254aa201c9d4b84e8
NICOLAS ANTONIO FERNANDEZ MILLAR <n.fernandez07@ufromail.cl>
2024-07-26 16:44:11 -0400

    Initial commit

---
+++ electivehelper-api/src/main/java/cl/ufro/dci/electivehelper_api/ElectivehelperApiApplication.java
@@ -0,0 +1,13 @@
+package cl.ufro.dci.electivehelper_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectivehelperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectivehelperApiApplication.class, args);
+	}
+
+}
---
+++ electivehelper-api/src/test/java/cl/ufro/dci/electivehelper_api/ElectivehelperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ufro.dci.electivehelper_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectivehelperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
