3d9e10186d7e86a41603ce1379582288d94d4ef3
NICOLAS IGNACIO PACHECO TOLEDO <n.pacheco02@ufromail.cl>
2024-07-29 18:52:38 -0400

    Add CrossOrigin annotation to ElectiveController and load electives in InteractiveScheduleComponent

--- electivehelper-api/src/main/java/cl/ufro/dci/electivehelper_api/controller/ElectiveController.java
+++ electivehelper-api/src/main/java/cl/ufro/dci/electivehelper_api/controller/ElectiveController.java
@@ -9,9 +9,10 @@ import cl.ufro.dci.electivehelper_api.service.ElectiveService;
 import java.util.ArrayList;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/elective")
 public class ElectiveController {
--- electivehelper-ui/src/app/interactive-schedule/interactive-schedule.component.ts
+++ electivehelper-ui/src/app/interactive-schedule/interactive-schedule.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Period } from '../domain/period';
 import { Slot } from '../domain/slot';
 import { CommonModule } from '@angular/common';
+import { Elective } from '../domain/elective';
+import { ElectiveService } from '../service/elective.service';
 
 @Component({
   selector: 'app-interactive-schedule',
@@ -15,6 +17,13 @@ export class InteractiveScheduleComponent {
   days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
   periods: Period[] = Period.getPeriods();
   selectedSlots: Slot[] = [];
+  electiveList: Elective[] = [];
+
+  constructor(private electiveService: ElectiveService){}
+
+  ngOnInit() {
+    this.loadElectives();
+  }
 
   selectSlot(day: string, period: Period) {
     let slot = new Slot(period.periodName, day, period.startTime, period.endTime);
@@ -39,5 +48,12 @@ export class InteractiveScheduleComponent {
   isSlotSelected(day: string, period: Period): boolean {
     let slot = new Slot(period.periodName, day, period.startTime, period.endTime);
     return this.selectedSlots.some(selectedSlot => selectedSlot.equals(slot));
+  }
+
+  loadElectives() {
+    this.electiveService.getAllElectives().subscribe({
+      next: (electives: Elective[]) => {this.electiveList = electives;},
+      error: (error) => {console.error('Error loading electives: ', error);}
+    });
   }
 }
