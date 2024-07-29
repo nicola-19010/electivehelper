dc410470eb00597e9962deb788d0a1f4240093f2
NICOLAS IGNACIO PACHECO TOLEDO <n.pacheco02@ufromail.cl>
2024-07-28 20:07:57 -0400

    Add interactive-schedule component files (basic structure)

---
+++ electivehelper-ui/src/app/interactive-schedule/interactive-schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interactive-schedule',
+  standalone: true,
+  imports: [],
+  templateUrl: './interactive-schedule.component.html',
+  styleUrl: './interactive-schedule.component.css'
+})
+export class InteractiveScheduleComponent {
+
+}
